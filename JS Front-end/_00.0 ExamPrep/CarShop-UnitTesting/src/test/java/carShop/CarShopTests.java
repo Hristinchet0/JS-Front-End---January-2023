08fe5e237b5ab48c7d9adb3250c66a1ae0808bd4
Hristinchet0 <bansko.sport@gmail.com>
2023-05-02 09:53:12 +0300

    Add

---
+++ JS Front-end/_00.0 ExamPrep/CarShop-UnitTesting/src/test/java/carShop/CarShopTests.java
@@ -0,0 +1,20 @@
+package carShop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarShopTests {
+
+   @Test
+    public void testCarShop() {
+
+   }
+}
+
+
+
